6244bb23c0f11725e4c806a660a8aed62f3ca27f
Nguyen Khanh Duy <ricky.ngk@gmail.com>
2016-04-04 10:33:10 +0700

    import sample 1

---
+++ sample1/app/src/main/java/com/rickyngk/databinding/sample1/MainActivity.java
@@ -0,0 +1,19 @@
+package com.rickyngk.databinding.sample1;
+
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.rickyngk.databinding.sample1.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        ActivityMainBinding binding = DataBindingUtil.setContentView(
+                this, R.layout.activity_main);
+        binding.setViewModel(new SampleViewModel());
+    }
+}
---
+++ sample1/app/src/main/java/com/rickyngk/databinding/sample1/SampleViewModel.java
@@ -0,0 +1,17 @@
+package com.rickyngk.databinding.sample1;
+
+/**
+ * Created by duynk on 4/4/16.
+ *
+ */
+public class SampleViewModel {
+    private String text = "This is a sample";
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
